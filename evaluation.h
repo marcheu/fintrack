eabe3a646fe4ad11219d6a664186abe358a0b817
Stéphane Marchesin <stephane.marchesin@gmail.com>
2021-01-03 16:08:10 -0800

    Separate portfolio evaluation into its own file

---
+++ evaluation.h
@@ -0,0 +1,9 @@
+#ifndef _EVALUATION_H_
+#define _EVALUATION_H_
+
+#include "data_series.h"
+#include "portfolio.h"
+
+void evaluate_portfolio (std::vector < data_series > &data, portfolio & p);
+
+#endif
