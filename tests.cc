c4c17b05aa4a4ed62c7a7573faad0b4fb747edf6
Stéphane Marchesin <stephane.marchesin@gmail.com>
2021-01-24 22:06:26 -0800

    Improve tests, add performance tests for different sizes

--- tests.cc
+++ tests.cc
@@ -8,21 +8,22 @@ static void run_cpu_gpu_tests (std::vector < data_series > data)
 {
 	printf ("=== CPU/GPU correctness tests ===\n");
 
+	int num_rounds = 1 << 17;
+	float cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation;
+	float gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation;
+	portfolio p;
+
+	monte_carlo m_cpu (data, false);
+	monte_carlo m_gpu (data, true);
+
 	for (int i = 0; i < 10; i++) {
-		portfolio p;
 		p.randomize (data);
 		p.normalize ();
 
-		int num_rounds = 1 << 16;
-		float cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation;
-		float gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation;
-
 		// Run the same tests on the CPU and on the GPU and compare the results
-		monte_carlo m_cpu (data, false);
 		m_cpu.run (p, cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation, num_rounds);
 		printf ("CPU monte carlo: e = %f σ = %f σd = %f σd75 = %f \n", cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation);
 
-		monte_carlo m_gpu (data, true);
 		m_gpu.run (p, gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation, num_rounds);
 		printf ("GPU monte carlo: e = %f σ = %f σd = %f σd75 = %f \n", gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation);
 
@@ -30,6 +31,25 @@ static void run_cpu_gpu_tests (std::vector < data_series > data)
 		assert (fabs (cpu_standard_deviation - gpu_standard_deviation) < 0.000001f);
 		assert (fabs (cpu_downside_deviation - gpu_downside_deviation) < 0.000001f);
 		assert (fabs (cpu_downsize_75_deviation - gpu_downsize_75_deviation) < 0.000001f);
+	}
+
+	num_rounds = 1 << 20;
+
+	for (int i = 0; i < 4; i++) {
+		p.randomize (data);
+		p.normalize ();
+
+		m_cpu.run (p, cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation, num_rounds);
+		printf ("CPU monte carlo: e = %f σ = %f σd = %f σd75 = %f \n", cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation);
+
+		m_gpu.run (p, gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation, num_rounds);
+		printf ("GPU monte carlo: e = %f σ = %f σd = %f σd75 = %f \n", gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation);
+
+		assert (fabs (cpu_expectancy - gpu_expectancy) < 0.000001f);
+		assert (fabs (cpu_standard_deviation - gpu_standard_deviation) < 0.000001f);
+		assert (fabs (cpu_downside_deviation - gpu_downside_deviation) < 0.000001f);
+		assert (fabs (cpu_downsize_75_deviation - gpu_downsize_75_deviation) < 0.000001f);
+
 	}
 }
 
@@ -49,22 +69,47 @@ static void run_performance_tests (std::vector < data_series > data)
 	uint64_t before, after;
 
 	monte_carlo m_cpu (data, false);
+	monte_carlo m_gpu (data, true);
 
 	before = get_time_us ();
 	for (int i = 0; i < 10; i++)
 		m_cpu.run (p, cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation, num_rounds);
 	after = get_time_us ();
 
-	printf ("CPU monte carlo: %f simulations per second\n", 10.f * num_rounds / ((after - before) / 1000000.0f));
-
-	monte_carlo m_gpu (data, true);
+	printf ("[%d rounds] CPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
 
 	before = get_time_us ();
 	for (int i = 0; i < 10; i++)
 		m_gpu.run (p, gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation, num_rounds);
 	after = get_time_us ();
 
-	printf ("GPU monte carlo: %f simulations per second\n", 10.f * num_rounds / ((after - before) / 1000000.0f));
+	printf ("[%d rounds] GPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
+
+	num_rounds = 1 << 17;
+	before = get_time_us ();
+	m_cpu.run (p, cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation, num_rounds);
+	after = get_time_us ();
+
+	printf ("[%d rounds] CPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
+
+	before = get_time_us ();
+	m_gpu.run (p, gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation, num_rounds);
+	after = get_time_us ();
+
+	printf ("[%d rounds] GPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
+
+	num_rounds = 1 << 18;
+	before = get_time_us ();
+	m_cpu.run (p, cpu_expectancy, cpu_standard_deviation, cpu_downside_deviation, cpu_downsize_75_deviation, num_rounds);
+	after = get_time_us ();
+
+	printf ("[%d rounds] CPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
+
+	before = get_time_us ();
+	m_gpu.run (p, gpu_expectancy, gpu_standard_deviation, gpu_downside_deviation, gpu_downsize_75_deviation, num_rounds);
+	after = get_time_us ();
+
+	printf ("[%d rounds] GPU monte carlo: %f simulations per second\n", num_rounds, 10.f * num_rounds / ((after - before) / 1000000.0f));
 }
 
 static void run_expectancy_tests (std::vector < data_series > data)
