b836882d800f55de79fb3012a8cfc4f254e871b1
Stéphane Marchesin <stephane.marchesin@gmail.com>
2021-02-19 23:19:26 -0800

    Fix biases in the 3x generation

--- make_3x.cc
+++ make_3x.cc
@@ -12,9 +12,10 @@ void make_3x (std::vector < data_series > data, portfolio & p, char *filename)
 	for (int day = 0; day < data[0].size - 1; day++) {
 		value = 0.;
 		for (unsigned stock = 0; stock < data.size (); stock++) {
-			// Each day, assume perfect 3x leverage, and skim 0.98 annualized fees
-			p.proportions[stock] *= (1. + (data[stock].values[day + 1] / data[stock].values[day] - 1.) * 3.) / pow (1.0098, 1. / 253.);
 			value += p.proportions[stock];
+			// Each day, assume perfect 3x leverage, and skim 0.98 annualized fees
+			// 1.0098 is the right value below, but 1.02 is a value that accounts better for slippage etc.
+			p.proportions[stock] *= (1. + (data[stock].values[day + 1] / data[stock].values[day] - 1.) * 3.) / pow (1.02, 1. / 253.);
 		}
 		fprintf (f, "%04d-%02d-%02d,", data[0].dates[day].year, data[0].dates[day].month, data[0].dates[day].day);
 		fprintf (f, "%f,%f,%f,%f,%f,%f\n", value, value, value, value, value, value);
